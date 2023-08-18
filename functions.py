e0e822beda1e8ebea19496c84953ed0979d4822d
jose_arevaloz <jose.arevaloz@fii-na.com>
2023-08-18 12:38:00 -0600

    class created

---
+++ functions.py
@@ -0,0 +1,7 @@
+class Payment:
+    def __init__(self, recipient, amount):
+        self.recipient = recipient
+        self.amount = amount
+
+payment1 = Payment('A', 25) 
+payment2 = Payment('B', 26)
