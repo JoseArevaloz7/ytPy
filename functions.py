73a4ce2c286538919570cdd3b10da82b66e81cce
jose_arevaloz <jose.arevaloz@fii-na.com>
2023-08-18 13:43:39 -0600

    add lists.py inheretance.py

--- functions.py
+++ functions.py
@@ -1,7 +1,23 @@
 class Payment:
+    initial_balance = 1000
+    
     def __init__(self, recipient, amount):
         self.recipient = recipient
         self.amount = amount
+    def check_balance(self):
+        return Payment.initial_balance - self.amount
+    
+    @classmethod
+    def update_initial_balance(cls, new_balance):
+        cls.initial_balance = new_balance
+    
+    @staticmethod
+    def check_valid_transaction(hour):
+        if hour >= 8 and hour <= 17:
+            return True
+        return False
+    
 
 payment1 = Payment('A', 25) 
-payment2 = Payment('B', 26)
+Payment.update_initial_balance(32)
+print(payment1.initial_balance)
